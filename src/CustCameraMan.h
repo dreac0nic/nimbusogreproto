7f891d4940914709470515c71aed69f1b349e852
Mark Smullen <mark@techfocus.net>
2014-02-03 06:13:32 -0600

    Moved header file

--- src/CustCameraMan.h
+++ src/CustCameraMan.h
@@ -3,49 +3,51 @@
 
 #include "Ogre.h"
 #include <limits>
-#include "SdkCameraMan.h"
 
-class CustCameraMan : public OgreBites::SdkCameraMan
+namespace OgreBites
 {
-	virtual bool frameRenderingQueued(const Ogre::FrameEvent& evt)
+	class CustCameraMan : SdkCameraMan
 	{
-		if (mStyle == OgreBites::CS_FREELOOK)
+		virtual bool frameRenderingQueued(const Ogre::FrameEvent& evt)
 		{
-			// build our acceleration vector based on keyboard input composite
-			Ogre::Vector3 accel = Ogre::Vector3::ZERO;
-			if (mGoingForward) accel += Ogre::Vector3(mCamera->getDirection().x, 0, mCamera->getDirection().z);
-			if (mGoingBack) accel -= Ogre::Vector3(mCamera->getDirection().x, 0, mCamera->getDirection().z);
-			if (mGoingRight) accel += mCamera->getRight();
-			if (mGoingLeft) accel -= mCamera->getRight();
-			if (mGoingUp) accel += mCamera->getUp();
-			if (mGoingDown) accel -= mCamera->getUp();
-
-			// if accelerating, try to reach top speed in a certain time
-			Ogre::Real topSpeed = mFastMove ? mTopSpeed * 20 : mTopSpeed;
-			if (accel.squaredLength() != 0)
+			if (mStyle == CS_FREELOOK)
 			{
-				accel.normalise();
-				mVelocity += accel * topSpeed * evt.timeSinceLastFrame * 10;
+				// build our acceleration vector based on keyboard input composite
+				Ogre::Vector3 accel = Ogre::Vector3::ZERO;
+				if (mGoingForward) accel += Ogre::Vector3(mCamera->getDirection().x, 0, mCamera->getDirection().z);
+				if (mGoingBack) accel -= Ogre::Vector3(mCamera->getDirection().x, 0, mCamera->getDirection().z);
+				if (mGoingRight) accel += mCamera->getRight();
+				if (mGoingLeft) accel -= mCamera->getRight();
+				if (mGoingUp) accel += mCamera->getUp();
+				if (mGoingDown) accel -= mCamera->getUp();
+
+				// if accelerating, try to reach top speed in a certain time
+				Ogre::Real topSpeed = mFastMove ? mTopSpeed * 20 : mTopSpeed;
+				if (accel.squaredLength() != 0)
+				{
+					accel.normalise();
+					mVelocity += accel * topSpeed * evt.timeSinceLastFrame * 10;
+				}
+				// if not accelerating, try to stop in a certain time
+				else mVelocity -= mVelocity * evt.timeSinceLastFrame * 10;
+
+				Ogre::Real tooSmall = std::numeric_limits<Ogre::Real>::epsilon();
+
+				// keep camera velocity below top speed and above epsilon
+				if (mVelocity.squaredLength() > topSpeed * topSpeed)
+				{
+					mVelocity.normalise();
+					mVelocity *= topSpeed;
+				}
+				else if (mVelocity.squaredLength() < tooSmall * tooSmall)
+					mVelocity = Ogre::Vector3::ZERO;
+
+				if (mVelocity != Ogre::Vector3::ZERO) mCamera->move(mVelocity * evt.timeSinceLastFrame);
 			}
-			// if not accelerating, try to stop in a certain time
-			else mVelocity -= mVelocity * evt.timeSinceLastFrame * 10;
 
-			Ogre::Real tooSmall = std::numeric_limits<Ogre::Real>::epsilon();
-
-			// keep camera velocity below top speed and above epsilon
-			if (mVelocity.squaredLength() > topSpeed * topSpeed)
-			{
-				mVelocity.normalise();
-				mVelocity *= topSpeed;
-			}
-			else if (mVelocity.squaredLength() < tooSmall * tooSmall)
-				mVelocity = Ogre::Vector3::ZERO;
-
-			if (mVelocity != Ogre::Vector3::ZERO) mCamera->move(mVelocity * evt.timeSinceLastFrame);
+			return true;
 		}
-
-		return true;
-	}
-};
+	};
+}
 
 #endif
