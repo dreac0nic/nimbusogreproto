308d58ad361dd8ac6e980c5e93239fdd8de06b84
drac0nic <spenser.m.bray@gmail.com>
2014-02-05 10:02:47 -0600

    I realized why I like some things.
    [Added preliminary memebers to WorldManager definition.]

--- src/WorldManager.h
+++ src/WorldManager.h
@@ -1,6 +1,8 @@
 #ifndef __NIMBUS_WORLDMANAGER
 #define __NIMBUS_WORLDMANAGER
 
+#include <vector>
+
 #include <OgreRoot.h>
 
 namespace Nimbus
@@ -9,7 +11,24 @@ namespace Nimbus
 
 	class WorldManager
 	{
-		// WORLD MAN
+		// MEMBER ATTRIBUTES
+	private:
+		// Represents the world's tile map with a vector of vectors of [chars]. (NOTE: Temp until tile class exists.)
+		std::vector< std::vector<char> > tiles;
+
+		// Represents our map's vector field with a vector of vectores of ogre vectors.
+		std::vector< std::vector<Ogre::Vector2> > vectors;
+
+		// MEMBER METHODS
+	public:
+		// CONSTRUCTORS --
+		WorldManager();
+
+		// DESTRUCTOR
+		~WorldManager();
+
+		// MAINTENANCE FUNCTIONS
+		void update(void);
 	};
 }
 
