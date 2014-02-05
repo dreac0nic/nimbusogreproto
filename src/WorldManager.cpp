d7bb0b5d1ff6488b6da6c887b837abdeb99afae4
drac0nic <spenser.m.bray@gmail.com>
2014-02-05 10:07:20 -0600

    See, I've always loved C++.
    [Added simple implementation in WorldManager.cpp]

---
+++ src/WorldManager.cpp
@@ -0,0 +1,27 @@
+#include "WorldManager.h"
+
+namespace Nimbus
+{
+	// CONSTRUCTORS -----------------------------------
+	WorldManager::WorldManager(void)
+	{
+		// Constructor
+	}
+
+	// DESTRUCTOR   -----------------------------------
+	WorldManager::~WorldManager(void)
+	{
+		// Destructor
+	}
+
+	// MAINTENANCE FUNCTIONS --------------------------
+	/* update function runs an update cycle on each of its
+	 * delegates.
+	 */
+	void WorldManager::update(void)
+	{
+		// Update Vectors
+		// Update Clouds
+		// Update Tiles
+	}
+}
