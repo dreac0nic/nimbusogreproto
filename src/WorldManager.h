e09610f7c65c8b4190d94ef61efea476e7be8f74
drac0nic <spenser.m.bray@gmail.com>
2014-02-05 09:30:25 -0600

    Added basic W O R L D M A N.

---
+++ src/WorldManager.h
@@ -0,0 +1,16 @@
+#ifndef __NIMBUS_WORLDMANAGER
+#define __NIMBUS_WORLDMANAGER
+
+#include <OgreRoot.h>
+
+namespace Nimbus
+{
+	using namespace Ogre;
+
+	class WorldManager
+	{
+		// WORLD MAN
+	};
+}
+
+#endif
